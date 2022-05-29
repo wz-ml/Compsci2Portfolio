a4c2c2d16adcadc4de5d3b4d3c77b5c1b81fd966
wz-ml <zhou.pe@wcs-g.com>
2022-05-29 16:53:07 -0600

    Finished Computer Science II - Concepts and Practice

--- src/Components/Modules/CompSciII.js
+++ src/Components/Modules/CompSciII.js
@@ -1,23 +1,28 @@
-import NavbarModule from "../Navbar"
-import React from "react"
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 import { NotionRenderer } from "react-notion";
-import response from "./page_1_response.json";
+import responsept1 from "./page_1_response.json";
+import responsept2 from "./page_1_response_pt2.json";
+import 'rc-dropdown/assets/index.css'
+import 'katex/dist/katex.min.css'
+import NavbarModule from "../Navbar"
+import React from "react"
 
-const blockMap = response;
+const blockMap1 = responsept1;
+const blockMap2 = responsept2;
 export default function CompSciII({Component, pageProps}) {
     return (
         <main>
             <NavbarModule />
-            <div style={{margin:150}}><h1>Computer Science II, Course CSE2010</h1></div>
+            <div style={{margin:150}}><h1>💻 Computer Science II, Course CSE2010</h1></div>
             <div style={{margin: 150, maxWidth: 768, justifyContent: 'center', alignItems: 'center' }}>
-                <NotionRenderer blockMap = {blockMap} />
+                <NotionRenderer blockMap = {blockMap1} />
             </div>
             <div style={{margin:150}}>
-                <h2>Project</h2>
-                <h5>To see the code, press the "show files" button!</h5>
-                <iframe title="BinarySearchEmbed" frameborder="0" width="100%" height="1000px" src="https://replit.com/@WilliamZhou18/BinarySearch?embed=true"></iframe></div>
+                <iframe title="BinarySearchEmbed" frameborder="0" width="100%" height="1000px" src="https://replit.com/@WilliamZhou18/BinarySearch?embed=true"></iframe>
+                <NotionRenderer blockMap = {blockMap2} />
+            </div>
+
         </main>
     )
 }

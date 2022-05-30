d8f2fffe9213d0c398adf78c386a5aa73d877ba3
wz-ml <zhou.pe@wcs-g.com>
2022-05-30 15:39:54 -0600

    Added extra writeup info to the Procedural Programming page.

--- src/Components/Modules/ProceduralProgrammingI.js
+++ src/Components/Modules/ProceduralProgrammingI.js
@@ -7,8 +7,10 @@ import NavbarModule from "../Navbar"
 import React from "react"
 import {Container} from "react-bootstrap"
 
-import response from "./page_2_response.json";
-const blockMap = response;
+import response1 from "./page_2_response.json";
+import response2 from "./page_2_response_pt2.json";
+const blockMap1 = response1;
+const blockMap2 = response2;
 
 export default function ProceduralProgrammingI() {
     return (
@@ -19,11 +21,16 @@ export default function ProceduralProgrammingI() {
             </Container>
             <Container className = "d-flex justify-content-center mt-3 mb-3">
             <div style={{maxWidth: 768, justifyContent: 'center', alignItems: 'center' }}>
-                <NotionRenderer blockMap = {blockMap} />
+                <NotionRenderer blockMap = {blockMap1} />
             </div>
             </Container>
             <Container className = "d-flex justify-content-center mt-3 mb-3">
             <iframe title="BinarySearchEmbed" frameborder="0" width="80%" height="675px" src="https://replit.com/@WilliamZhou18/Connect4CPP?embed=true"></iframe>
+            </Container>
+            <Container className = "d-flex justify-content-center mt-3 mb-3">
+            <div style={{maxWidth: 768, justifyContent: 'center', alignItems: 'center' }}>
+                <NotionRenderer blockMap = {blockMap2} />
+            </div>
             </Container>
         </main>
     )
